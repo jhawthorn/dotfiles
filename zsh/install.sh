19aecf383f39487e5772b6b9af88b8220e4c1981
John Hawthorn <john.hawthorn@gmail.com>
2012-05-11 00:27:15 -0700

    oh my zsh install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,6 @@
 cd "$( dirname "$0" )"
 ln -sf "$(pwd)/.zshrc" "$HOME/.zshrc"
 ln -sf "$(pwd)/.zshenv" "$HOME/.zshenv"
+
+rm -Rf "$HOME/.oh-my-zsh"
+git clone https://github.com/sorin-ionescu/oh-my-zsh.git "$HOME/.oh-my-zsh"
