e21041a72ab935d20ddc49f54b0d94392d048a7b
John Hawthorn <john.hawthorn@gmail.com>
2012-12-11 03:34:31 -0800

    bin changes

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,5 @@
 cd "$( dirname "$0" )"
+mkdir "$HOME/bin"
 for name in autodisplay.rb  chrome  dunst  inspect  install.sh  mplayer  mplayer-youtube  rline  roll  starcraft  statusbar  urlview  watchmsg  weechat; do
 	ln -sf "$(pwd)/$name" "$HOME/bin/$name"
 done
