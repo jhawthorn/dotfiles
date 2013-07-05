3575f3568ef872dee95ba4a236053d7ece76b83d
John Hawthorn <john.hawthorn@gmail.com>
2013-07-05 13:11:27 -0700

    add hawth theme

--- awesome/install.sh
+++ awesome/install.sh
@@ -1,6 +1,7 @@
 cd "$( dirname "$0" )"
 
-mkdir -p "$HOME/.config/awesome"
+mkdir -p "$HOME/.config/awesome/themes"
 
 ln -sf "$(pwd)/rc.lua" "$HOME/.config/awesome/rc.lua"
+ln -sf "$(pwd)/hawth" "$HOME/.config/awesome/themes/hawth"
 
--- awesome/rc.lua
+++ awesome/rc.lua
@@ -353,8 +353,8 @@ awful.rules.rules = {
 client.connect_signal("manage", function (c, startup)
     -- Enable sloppy focus
     c:connect_signal("mouse::enter", function(c)
-        if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
-            and awful.client.focus.filter(c) then
+        if (awful.layout.get(c.screen) ~= awful.layout.suit.magnifier) and
+		awful.client.focus.filter(c) then
             client.focus = c
         end
     end)
