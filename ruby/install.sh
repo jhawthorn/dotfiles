b7411973238ef5c0df7c7868011a47a464204f5c
John Hawthorn <john.hawthorn@gmail.com>
2014-01-19 17:50:11 -0800

    ensure ~/.local/share exists

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,6 +3,8 @@ ln -sf "$(pwd)/.gemrc" "$HOME/.gemrc"
 ln -sf "$(pwd)/.irbrc" "$HOME/.irbrc"
 ln -sf "$(pwd)/.pryrc" "$HOME/.pryrc"
 ln -sf "$(pwd)/.xrayconfig" "$HOME/.xrayconfig"
+
+mkdir -p "$HOME/.local/share"
 ln -sf "$(pwd)/chruby/share/chruby" "$HOME/.local/share/chruby"
 ln -sf "$(pwd)/bundler-exec" "$HOME/.local/share/bundler-exec"
 
