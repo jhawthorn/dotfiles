184ab0908fc223981c7438b1b5b41e62389655f4
John Hawthorn <john.hawthorn@gmail.com>
2017-03-11 18:33:51 -0800

    Only install ruby 2.4.0

--- ruby/install_rubies.sh
+++ ruby/install_rubies.sh
@@ -1,7 +1,4 @@
 cd ruby-install
-export NO_INSTALL_DEPS=1
 export CFLAGS="-O3 -march=native -g"
 
-bin/ruby-install ruby 2.0.0
-bin/ruby-install ruby 1.9.3
-bin/ruby-install ruby 2.1.0
+bin/ruby-install ruby 2.4.0
