ed8f10f9fb8e06d8404bce765d3447f36b1ce0ac
John Hawthorn <john.hawthorn@gmail.com>
2017-03-11 21:50:50 -0800

    Install awesome configs by default

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/zsh
 
 cd "$( dirname "$0" )"
-zsh    bin/install.sh
-zsh    git/install.sh
-zsh   mail/install.sh
-zsh  music/install.sh
-zsh   misc/install.sh
-zsh   ruby/install.sh
-zsh    vim/install.sh
-zsh      x/install.sh
-zsh    zsh/install.sh
+zsh awesome/install.sh
+zsh     bin/install.sh
+zsh     git/install.sh
+zsh    mail/install.sh
+zsh   music/install.sh
+zsh    misc/install.sh
+zsh    ruby/install.sh
+zsh     vim/install.sh
+zsh       x/install.sh
+zsh     zsh/install.sh
 ln -sfn "$(pwd)/terminfo" "$HOME/.terminfo"
