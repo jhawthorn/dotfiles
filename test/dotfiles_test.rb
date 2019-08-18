468f276e829425cc5ff7f3e7181f8f5a303b2540
John Hawthorn <john@hawthorn.email>
2019-08-17 18:57:46 -0700

    Add a test

---
+++ test/dotfiles_test.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env ruby
+
+require "minitest"
+require "minitest/autorun"
+require "ttytest"
+
+require "socket"
+
+class FzyTest < Minitest::Test
+  def test_shell
+    @tty = new_terminal
+    @tty.assert_matches <<TERM
+
+#{expected_ps1}
+$
+TERM
+  end
+
+  def test_vim
+    @tty = new_terminal
+    @tty.send_keys("vim\n")
+    @tty.assert_matches <<TERM
+  1
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+[No Name]                                                     0,0-1          All
+TERM
+  end
+
+  private
+
+  def expected_ps1
+    hostname = Socket.gethostname
+    user = ENV["USER"]
+    pwd = "/"
+    ruby = "ruby #{RUBY_VERSION}"
+
+    "#{user}@#{hostname}:#{pwd}  [#{ruby}]"
+  end
+
+  def new_terminal
+    Dir.chdir("/") do
+      TTYtest.new_terminal("/bin/zsh -l")
+    end
+  end
+end
