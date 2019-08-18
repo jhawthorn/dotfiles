d99d6907f890e38445124aa657ea317ad3046b13
John Hawthorn <john@hawthorn.email>
2019-08-17 18:44:16 -0700

    Modern ruby versions

--- ruby/install_rubies.sh
+++ ruby/install_rubies.sh
@@ -2,6 +2,7 @@ cd ruby-install
 export NO_INSTALL_DEPS=1
 export CFLAGS="-O3 -march=native -g"
 
-bin/ruby-install ruby 2.0.0
-bin/ruby-install ruby 1.9.3
-bin/ruby-install ruby 2.1.0
+#bin/ruby-install ruby 1.9.3
+#bin/ruby-install ruby 2.5.1
+#bin/ruby-install ruby 2.5
+bin/ruby-install ruby 2.6
