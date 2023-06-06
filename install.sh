3c820b08bb94abb939840a51d255c5793cdb254c
John Hawthorn <john@hawthorn.email>
2023-06-06 16:01:15 -0700

    Update neovim

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ if [[ -n "$CODESPACES" ]]; then
 	echo 0 | sudo tee /proc/sys/kernel/kptr_restrict > /dev/null
 	echo -1 | sudo tee /proc/sys/kernel/perf_event_paranoid > /dev/null
 
-	curl -sSL https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.tar.gz | tar xvzp --strip-components=1 -C "$HOME/.local/"
+	curl -sSL https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz | tar xvzp --strip-components=1 -C "$HOME/.local/"
 	ln -sf "$HOME/.local/bin/nvim" "$HOME/bin/"
 fi
 
