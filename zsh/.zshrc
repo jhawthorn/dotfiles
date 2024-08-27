16b7e0a42db81e0e7f226276e51b5b166bfae1b8
John Hawthorn <john@hawthorn.email>
2024-08-27 13:08:25 -0700

    Cleanup prompt

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -29,10 +29,6 @@ tmgem() { tmux new-window -c `bundle show $1` -n "$1" }
 # Expands symlinks on cd
 setopt CHASE_LINKS
 
-PS1='
-%F{135}%n%f@%F{166}%m%f:%F{118}%~%f ${vcs_info_msg_0_} [${RUBY_ENGINE} ${RUBY_VERSION}]
-$ '
-
 export GOPATH=~/go
 
 export QUOTING_STYLE=literal
--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -9,8 +9,15 @@ zstyle ':vcs_info:*' actionformats "(%b %m%c%u %F{cyan}%a%f)"
 zstyle ':vcs_info:*' stagedstr "%F{green}S%f"
 zstyle ':vcs_info:*' unstagedstr "%F{yellow}U%f"
 
-precmd() {
+prompt_hawth_precmd() {
 	vcs_info
 }
+
+precmd_functions+=(prompt_hawth_precmd)
+
+PS1='
+%F{135}%n%f@%F{166}%m%f:%F{118}%~%f ${vcs_info_msg_0_} [${RUBY_ENGINE} ${RUBY_VERSION}]
+$ '
+
 
 setopt prompt_subst
