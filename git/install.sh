9e872e98e0ad20640e2fdc46b379138dc8b18449
John Hawthorn <john@hawthorn.email>
2024-10-31 17:05:19 -0700

    Add gh config to dotfiles

--- git/install.sh
+++ git/install.sh
@@ -2,3 +2,6 @@ cd "$( dirname "$0" )"
 ln -sf "$(pwd)/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$(pwd)/.gitignore" "$HOME/.gitignore"
 
+mkdir -p "$HOME/.config/gh"
+ln -sf "$(pwd)/gh_config.yml" "$HOME/.config/gh/config.yml"
+
