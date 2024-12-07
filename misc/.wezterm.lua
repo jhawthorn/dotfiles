bce6370b399ee4e20219940e38d50b6d81c6b252
John Hawthorn <john@hawthorn.email>
2024-12-07 13:25:09 -0800

    Slightly more transparent

--- misc/.wezterm.lua
+++ misc/.wezterm.lua
@@ -52,7 +52,8 @@ config.colors = {
   visual_bell = '#202020',
 }
 
-config.window_background_opacity = 0.85
+--config.window_background_opacity = 0.85
+config.window_background_opacity = 0.80
 config.macos_window_background_blur = 30
 
 -- config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
