01bbb09b01d03c2b8c975357ef2737ac1285788f
John Hawthorn <john@hawthorn.email>
2025-08-11 09:21:10 -0700

    Add ghostty

--- misc/install.sh
+++ misc/install.sh
@@ -2,4 +2,5 @@ cd "$( dirname "$0" )"
 ln -sf "$(pwd)/.agignore" "$HOME/.agignore"
 ln -sf "$(pwd)/.wezterm.lua" "$HOME/.wezterm.lua"
 ln -sf "$(pwd)/.inputrc" "$HOME/.inputrc"
+ln -sf "$(pwd)/.ghostty" "$HOME/.config/ghostty/config"
 
