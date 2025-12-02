e366e69429c7dafd7ceb41684af5577680a83b7d
John Hawthorn <john@hawthorn.email>
2025-12-01 21:33:18 -0800

    Move install-ruby to .local/share

--- ruby/install_rubies.sh
+++
@@ -1,8 +0,0 @@
-cd ruby-install
-export NO_INSTALL_DEPS=1
-export CFLAGS="-O3 -march=native -g"
-
-#bin/ruby-install ruby 1.9.3
-#bin/ruby-install ruby 2.5.1
-#bin/ruby-install ruby 2.5
-bin/ruby-install ruby 2.6
