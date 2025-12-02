732d868ea1924aed540e3ecb6e919992b2ffda26
John Hawthorn <john@hawthorn.email>
2025-12-01 20:43:54 -0800

    Haven't been using mutt recently

--- mail/install.sh
+++
@@ -1,8 +0,0 @@
-cd "$( dirname "$0" )"
-ln -sf "$(pwd)/.msmtprc"       "$HOME/.msmtprc"
-ln -sf "$(pwd)/.muttrc"        "$HOME/.muttrc"
-ln -sf "$(pwd)/.offlineimaprc" "$HOME/.offlineimaprc"
-
-mkdir -p "$HOME/.mutt"
-ln -sf "$(pwd)/mutt/colors.linux" "$HOME/.mutt/colors.linux"
-ln -sf "$(pwd)/mutt/mailcap"      "$HOME/.mutt/mailcap"
