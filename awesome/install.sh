97774ba0a7e4aae5fe21470357eafaca45df0bb5
John Hawthorn <john@hawthorn.email>
2025-12-01 20:45:44 -0800

    Remove awesome

--- awesome/install.sh
+++
@@ -1,7 +0,0 @@
-cd "$( dirname "$0" )"
-
-mkdir -p "$HOME/.config/awesome/themes"
-
-ln -sf "$(pwd)/rc.lua" "$HOME/.config/awesome/rc.lua"
-ln -sf "$(pwd)/hawth" "$HOME/.config/awesome/themes/hawth"
-
