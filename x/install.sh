90e8ee110cbb1da8a28b4342ec71f7dcc64b2eac
John Hawthorn <john@hawthorn.email>
2025-12-01 20:48:10 -0800

    Delete x for now

--- x/install.sh
+++
@@ -1,5 +0,0 @@
-cd "$( dirname "$0" )"
-ln -sf "$(pwd)/.xinitrc" "$HOME/.xinitrc"
-ln -sf "$(pwd)/.xsession" "$HOME/.xsession"
-ln -sf "$(pwd)/.Xresources" "$HOME/.Xresources"
-ln -sf "$(pwd)/.xbindkeysrc" "$HOME/.xbindkeysrc"
