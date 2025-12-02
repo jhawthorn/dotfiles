c9e001e2baa65f90f6afd960e0fdda3dce906436
John Hawthorn <john@hawthorn.email>
2025-12-01 20:51:15 -0800

    Update install scripts

--- bin/install.sh
+++ bin/install.sh
@@ -4,26 +4,13 @@ mkdir -p "$HOME/bin"
 cd `dirname "$0"`
 
 files=(
-autodisplay.rb
-bgvim
 chrome
-dmenu_mpd
-dmenu_run
-dmenu_surfraw
-dmenu_tv
-dunst
 git-recent
 inspect
 install_ruby
 rline
 roll
-rtorrent
-starcraft
-statusbar
-urlview
 update_dotfiles
-watchmsg
-weechat
 )
 
 for name in $files; do
--- install.sh
+++ install.sh
@@ -9,16 +9,12 @@ git submodule update --init --recursive
 zsh aerospace/install.sh
 zsh    bin/install.sh
 zsh    git/install.sh
-zsh   mail/install.sh
-zsh  music/install.sh
 zsh   misc/install.sh
 zsh   ruby/install.sh
 zsh   tmux/install.sh
 zsh    vim/install.sh
 zsh   nvim/install.sh
-zsh      x/install.sh
 zsh    zsh/install.sh
-ln -sfn "$(pwd)/terminfo" "$HOME/.terminfo"
 
 ln -sfn "$(pwd)" "$HOME/.dotfiles"
 
