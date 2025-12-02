6d9f69a6d2f1cf68e12c6ee52e1a2f27c7c6d090
John Hawthorn <john@hawthorn.email>
2025-12-01 20:44:11 -0800

    Haven't used these music programs recently

--- music/install.sh
+++
@@ -1,9 +0,0 @@
-cd "$( dirname "$0" )"
-
-mkdir -p "$HOME/.ncmpcpp"
-mkdir -p "$HOME/.config/beets"
-
-ln -sf "$(pwd)/ncmpcpp_config" "$HOME/.ncmpcpp/config"
-ln -sf "$(pwd)/beetsconfig.yaml" "$HOME/.config/beets/config.yaml"
-ln -sf "$(pwd)/mpdconf" "$HOME/.mpdconf"
-
